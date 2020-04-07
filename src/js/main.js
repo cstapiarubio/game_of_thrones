cb0e50137722b39d482f7c582c895408ae22ba09
carolina_tapia <cstapiarubio@gmail.com>
2020-04-07 16:03:30 -0400

    listo el modal

--- src/js/main.js
+++ src/js/main.js
@@ -1,20 +1,15 @@
 
-/*for(var i=1; i<=10;i++){ 
-    $('#card').append($('<img>').attr('id',i).attr('data-toggle', "modal").attr('data-target', '#exampleModal').attr('src', 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/' + i + '.png'))   
-}*/
-
     $('img').on('click', function(event) {
         var id =$(this).attr('id');
     $.get("https://anapioficeandfire.com/api/houses/" + id, function(res) {
         console.log(res);
         var nombre = res.name;
         var palabra = res.words;
-        /*var titulo = res.titles;*/
         console.log(nombre);
         console.log(palabra);
 
         for(var i = 0; i < res.titles.length; i++) {
-        var titulo= titulo + res.titles[i];
+        var titulo= titulo + "<br>" + res.titles[i];
     }
     console.log(titulo);
 
@@ -23,7 +18,6 @@
     $('.modal-body').append("<h4> Titulos: </h4>"+"<p>" + titulo + "</p>");
 
     });
-    //event.stopPropagation();
 });
 
 $('.close').on('click', function(event) {
